0a29a5673f06d5371d3f449ec060dc388a3624f2
Glauber <glauber.bannwart@gmail.com>
2023-06-14 18:48:16 -0300

    Passing Webpack and Primary Linter Check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './style.css';
 
 function component() {
   const element = document.createElement('div');
-
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
