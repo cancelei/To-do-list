9ec16a9e5e3b24baa98152273f003c7a065adfdc
Glauber <glauber.bannwart@gmail.com>
2023-06-15 16:02:20 -0300

    JS Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+// Array of objects (For test phase)
 
-  return element;
-}
+staticlistitems = [
+  {
+  id: 0, 
+  description: "Clean my room", 
+  status: 0 
+  },
+  {
+    id: 1,
+    description: "Do the dishes",
+    status: 0
+  },
+  {
+    id: 2,
+    description: "Take out the trash",
+    status: 0
+  },
+  {
+    id: 3,
+    description: "Walk the dog",
+    status: 0
+  },
+  {
+    id: 4,
+    description: "Do the laundry",
+    status: 0
+  },
+  {
+    id: 5,  
+    description: "Go to the gym",
+    status: 0
+  },]
 
-document.body.appendChild(component());
+  document.addEventListener("", () => {
+
+  const listparent = document.getElementsByClassName('listparent');
+  const listtitle = document.createElement('div');
+  listtitle.className = 'listtitle';
+  listparent.appendChild(listtitle);
+
+  const title = document.createElement('h2');
+  const refresher = document.createElement('refresher');
+
+  title.innerHTML = "To Do List";
+  title.className = 'title';
+  refresher.className = 'refresher';
+
+  listtitle.appendChild(title);
+  listtitle.appendChild(refresher);
+
+  });
